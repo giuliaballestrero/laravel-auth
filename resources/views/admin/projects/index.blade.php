1a16c027756784bfe244e4040551e8bc05479790
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-20 18:21:49 +0100

    Add Index page

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <table class="table table-striped table-borderless table-hover mt-5">
+        <thead>
+            <tr>
+                <th scope="col">#id</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Thumb</th>
+                <th scope="col">Creation Date</th>
+                <th scope="col">Type</th>
+                <th scope="col">Completed</th>
+                <th scope="col">
+                    <a href="" class="btn btn-success">
+                        Add new Project
+                    </a>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                <td>{{ $project->id }}</td>
+                <td>{{ $project->slug }}</td>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->description }}</td>
+                <td>{{ $project->thumb }}</td>
+                <td>{{ $project->creation_date }}</td>
+                <td>{{ $project->type }}</td>
+                <td>{{ $project->completed }}</td>
+                <td>
+                    <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-sm btn-primary">
+                        Show
+                    </a>
+
+                    <a href="" class="btn btn-sm btn-warning">
+                        Edit
+                    </a>
+                    
+                    {{-- per la delete inserire il bottone in un form --}}
+                    <a href="" class="btn btn-sm btn-danger">
+                        Delete
+                    </a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\ProjectController as ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,9 @@ Route::middleware(['auth', 'verified'])
     ->group(function() {
         Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
+        Route::resource('projects', ProjectController::class);
     });
+
+
 
 require __DIR__.'/auth.php';
